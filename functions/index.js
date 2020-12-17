a3e6cbfff25fefb9a28f9c1278b6629df5c16459
Helton Isac <helton.isac@gmail.com>
2020-12-17 16:27:38 -0300

    VS-CODE - Auto format - index.js

--- functions/index.js
+++ functions/index.js
@@ -1,81 +1,82 @@
-const express = require("express")
-const {ApolloServer, gql} = require("apollo-server-express")
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-const serviceAccount = require('./fiapbancographql-firebase-adminsdk-b531i-8746c847e2.json')
+const serviceAccount = require("./fiapbancographql-firebase-adminsdk-b531i-8746c847e2.json");
 
 admin.initializeApp({
-    credential:admin.credential.cert(serviceAccount),
-    databaseURL:"https://fiapbancographql-default-rtdb.firebaseio.com/"
-})
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fiapbancographql-default-rtdb.firebaseio.com/",
+});
 
 const typeDefs = gql`
-    type Produto{
-        id:Int
-        nomeproduto:String
-        descricao:String
-        fornecedor:String
-        preco:Float
-        datacadastro:String
-    }
-    type Query{
-        produto:[Produto]
-    }
-    type Mutation{
-        novoProduto(
-            id:Int
-            nomeproduto:String
-            descricao:String
-            fornecedor:String
-            preco:Float
-            datacadastro:String
-        ):[Produto]
-
-    }
-`
+  type Produto {
+    id: Int
+    nomeproduto: String
+    descricao: String
+    fornecedor: String
+    preco: Float
+    datacadastro: String
+  }
+  type Query {
+    produto: [Produto]
+  }
+  type Mutation {
+    novoProduto(
+      id: Int
+      nomeproduto: String
+      descricao: String
+      fornecedor: String
+      preco: Float
+      datacadastro: String
+    ): [Produto]
+  }
+`;
 const resolvers = {
-    Query:{
-        produto:()=>{
-            return admin.database()
-                   .ref("produtos")
-                   .once("value")
-                   .then(snap => snap.val())
-                   .then(val => Object.keys(val)
-                   .map((key)=>val[key]))
-        }
-    },
-    Mutation:{
-        novoProduto(_,{id,nomeproduto,descricao,fornecedor,preco,datacadastro}){
-            const novo = {
-            id:id,
-            nomeproduto:nomeproduto,
-            descricao:descricao,
-            fornecedor:fornecedor,
-            preco:preco,
-            datacadastro:datacadastro
-        }
-        admin.database()
+  Query: {
+    produto: () => {
+      return admin
+        .database()
         .ref("produtos")
-        .push(novo)
-        
-        return admin.database()
-                   .ref("produtos")
-                   .limitToLast(1)
-                   .once("value")
-                   .then(snap => snap.val())
-                   .then(val => Object.keys(val)
-                   .map((key)=>val[key]))
-    }
-}
-}
+        .once("value")
+        .then((snap) => snap.val())
+        .then((val) => Object.keys(val).map((key) => val[key]));
+    },
+  },
+  Mutation: {
+    novoProduto(
+      _,
+      { id, nomeproduto, descricao, fornecedor, preco, datacadastro }
+    ) {
+      const novo = {
+        id: id,
+        nomeproduto: nomeproduto,
+        descricao: descricao,
+        fornecedor: fornecedor,
+        preco: preco,
+        datacadastro: datacadastro,
+      };
+      admin.database().ref("produtos").push(novo);
 
-const app = express()
+      return admin
+        .database()
+        .ref("produtos")
+        .limitToLast(1)
+        .once("value")
+        .then((snap) => snap.val())
+        .then((val) => Object.keys(val).map((key) => val[key]));
+    },
+  },
+};
+
+const app = express();
 
 const server = new ApolloServer({
-    typeDefs,resolvers
-})
+  typeDefs,
+  resolvers,
+});
 
-server.applyMiddleware({app,path:"/",cors:true})
+server.applyMiddleware({ app, path: "/", cors: true });
 
-exports.graphql = functions.https.onRequest(app)
+exports.graphql = functions.https.onRequest(app);
